9a239dd32461b450f5fabb449cce5cc47e16c07e
c301hoon <hoon.c301@gmail.com>
2023-10-15 17:55:51 +0900

    Add: accounts models, admin
     #9

--- genie/server/backend/accounts/admin.py
+++ genie/server/backend/accounts/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from accounts.models import SocialAccount, Inbox
+from backend.utils.models import BaseModelAdmin
 
-# Register your models here.
+
+class SocialAccountAdmin(BaseModelAdmin):
+    list_display: tuple[str, str, str] = (
+        "id",
+        "nickname",
+        "pub_key",
+    )
+
+    list_display_links: tuple[str] = ("id",)
+    search_fields: tuple[str] = ("nickname",)
+
+
+class InboxAdmin(BaseModelAdmin):
+    list_display: tuple[str, str, str, str, str] = (
+        "id",
+        "account",
+        "network",
+        "sns",
+        "pub_key",
+    )
+
+    list_display_links: tuple[str] = ("id",)
+    search_fields: tuple[str, str, str] = (
+        "account__nickname",
+        "sns__name",
+        "network__name",
+    )
+
+
+admin.site.register(SocialAccount, SocialAccountAdmin)
+admin.site.register(Inbox, InboxAdmin)
+
+admin.site.site_header = "Genie Admin Site"
+admin.site.site_title = "Genie Admin Site"
+admin.site.index_title = "Genie Admin Site"
--- genie/server/backend/accounts/models.py
+++ genie/server/backend/accounts/models.py
@@ -1,3 +1,82 @@
+from typing import List, Type
 from django.db import models
+from backend.utils.models import BaseModel
+from backend.utils import errors
 
-# Create your models here.
+
+class SocialAccount(BaseModel):
+    class Meta:
+        verbose_name: str = "Genie user"
+        verbose_name_plural: str = "Genie users"
+
+    nickname: str = models.CharField(
+        verbose_name="nickname",
+        max_length=50,
+        blank=False,
+        null=False,
+        help_text="nickname used in genie service",
+    )
+
+    pub_key: str = models.CharField(
+        verbose_name="public key",
+        max_length=100,
+        blank=False,
+        null=False,
+        unique=True,
+        help_text="public key",
+    )
+
+    secret_key: str = models.CharField(
+        verbose_name="secret key",
+        max_length=400,
+        blank=False,
+        null=False,
+        help_text="secret key",
+    )
+
+    def __str__(self):
+        return f"{self.nickname} - {self.pub_key}"
+
+
+class Inbox(BaseModel):
+    class Meta:
+        verbose_name: str = "Inbox"
+        verbose_name_plural: str = "Inbox"
+        constraints: List[models.UniqueConstraint] = [
+            models.UniqueConstraint(
+                fields=["pub_key", "network", "sns"],
+                name="unique (pub_key, network, sns)",
+            ),
+        ]
+
+    pub_key: str = models.CharField(
+        verbose_name="public key",
+        max_length=100,
+        blank=False,
+        null=False,
+        unique=True,
+        help_text="public key",
+    )
+
+    secret_key: str = models.CharField(
+        verbose_name="secret key",
+        max_length=400,
+        blank=False,
+        null=False,
+        help_text="secret key",
+    )
+
+    account: "SocialAccount" = models.ForeignKey(
+        SocialAccount, on_delete=models.SET_NULL, related_name="inboxes", null=True
+    )
+
+    network: "Network" = models.ForeignKey(
+        "blockchain.Network", on_delete=models.PROTECT, related_name="inboxes"
+    )
+
+    sns: "SNS" = models.ForeignKey(
+        "sns.SNS", on_delete=models.PROTECT, related_name="inboxes"
+    )
+
+    def __str__(self):
+        return f"{self.account.nickname}({self.sns.name}) {self.pub_key}"
