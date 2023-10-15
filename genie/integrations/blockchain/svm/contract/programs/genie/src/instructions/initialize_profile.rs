3c1e1cfd6fbf6e79fc88908591ff0aefb54ab209
Deok <deok.c301@gmail.com>
2023-10-16 08:15:03 +0900

    Add printing Mark(sbt) logic
    - mint mark nft and freeze it to implement sbt
    - mark shows account belongs to genie system and it is social account
    
     On branch feature/#18
     Changes to be committed:
    	modified:   programs/genie/src/instructions/initialize_profile.rs

--- genie/integrations/blockchain/svm/contract/programs/genie/src/instructions/initialize_profile.rs
+++ genie/integrations/blockchain/svm/contract/programs/genie/src/instructions/initialize_profile.rs
@@ -2,7 +2,7 @@ use crate::{Genie, Profile};
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{Mint, Token, TokenAccount},
+    token::{freeze_account, mint_to, FreezeAccount, Mint, MintTo, Token, TokenAccount},
 };
 
 #[derive(Accounts)]
@@ -38,6 +38,50 @@ pub fn initialize_profile(ctx: Context<InitializeProfile>, profile_bump: u8) -> 
     ctx.accounts
         .profile
         .initialize(ctx.accounts.initial_auth.key(), profile_bump)?;
+
+    print_and_freeze_mark(ctx)?;
+
+    Ok(())
+}
+
+fn print_and_freeze_mark(ctx: Context<InitializeProfile>) -> Result<()> {
+    // mint mark (sbt) for social account
+    let bump = ctx.accounts.genie.bump;
+    let authority_seeds = &[
+        "genie".as_bytes(),
+        &ctx.accounts.genie.authority.to_bytes(),
+        &[bump],
+    ];
+
+    let cpi_program = ctx.accounts.token_program.to_account_info();
+    let cpi_accounts = MintTo {
+        mint: ctx.accounts.profile_mark.to_account_info(),
+        to: ctx.accounts.profile_mark_account.to_account_info(),
+        authority: ctx.accounts.genie.to_account_info(),
+    };
+
+    mint_to(
+        CpiContext::new(cpi_program, cpi_accounts).with_signer(&[&authority_seeds[..]]),
+        1,
+    )?;
+
+    // freeze mark (sbt) for social account
+    let bump = ctx.accounts.genie.bump;
+    let authority_seeds = &[
+        "genie".as_bytes(),
+        &ctx.accounts.genie.authority.to_bytes(),
+        &[bump],
+    ];
+    let cpi_program = ctx.accounts.token_program.to_account_info();
+    let cpi_accounts = FreezeAccount {
+        account: ctx.accounts.profile_mark_account.to_account_info(),
+        mint: ctx.accounts.profile_mark.to_account_info(),
+        authority: ctx.accounts.genie.to_account_info(),
+    };
+
+    freeze_account(
+        CpiContext::new(cpi_program, cpi_accounts).with_signer(&[&authority_seeds[..]]),
+    )?;
 
     Ok(())
 }
