55a86ef34477386b39d520f530f3b288a11b5aa3
Deok <gyu.c301@gmail.com>
2023-10-22 16:21:00 +0900

    Implement Inbox class & inbox initialization

---
+++ genie/integrations/blockchain/svm/ts/packages/svm-integration/src/inbox.ts
@@ -0,0 +1,73 @@
+import Genie from "./genie";
+import { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID } from "./utils";
+import { web3 } from "@coral-xyz/anchor";
+import { getAssociatedTokenAddressSync } from "@solana/spl-token";
+``;
+export default class Inbox {
+  genie: Genie;
+  initialAuth: web3.PublicKey;
+  isInitialized: boolean = false;
+
+  constructor(genie: Genie, initialAuth: web3.PublicKey) {
+    this.genie = genie;
+    this.initialAuth = initialAuth;
+  }
+
+  async initialize(initialAuthInboxKeypair: web3.Keypair) {
+    try {
+      const program = await this.genie.program;
+
+      if (program === undefined) {
+        throw new Error("Genie not initialized");
+      }
+      if (this.genie.inboxMark === undefined) {
+        throw new Error("Genie inboxMark not initialized");
+      }
+      const inboxData = await program.account.inbox
+        .fetch(this.key)
+        .then((res) => res)
+        .catch((err) => undefined);
+
+      if (inboxData !== undefined) {
+        this.isInitialized = true;
+        return this.key;
+      }
+
+      const tx = await program.methods
+        .initializeInbox()
+        .accounts({
+          inbox: this.key,
+          initialAuth: this.initialAuth,
+          inboxMarkAccount: this.inboxMarkAccount,
+          inboxMark: this.genie.inboxMark,
+          genie: this.genie.key,
+          payer: this.genie.client.payer.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+          systemProgram: web3.SystemProgram.programId,
+          rent: web3.SYSVAR_RENT_PUBKEY,
+        })
+        .signers([initialAuthInboxKeypair])
+        .rpc({ skipPreflight: true })
+        .then((res) => res)
+        .catch((error) => {
+          throw new Error("inbox initialization failed");
+        });
+      this.isInitialized = true;
+      return this.key;
+    } catch (err) {}
+  }
+
+  get key() {
+    return web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("inbox"), this.initialAuth.toBuffer()],
+      this.genie.programId
+    )[0];
+  }
+
+  get inboxMarkAccount() {
+    return this.genie.inboxMark
+      ? getAssociatedTokenAddressSync(this.genie.inboxMark, this.key, true)
+      : undefined;
+  }
+}
