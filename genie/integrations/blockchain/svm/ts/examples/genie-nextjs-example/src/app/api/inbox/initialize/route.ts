b8149397b1d803262208497d4edef7754868a5f9
Deok <gyu.c301@gmail.com>
2023-10-27 02:07:30 +0900

    Add inbox initialization routing & inbox ts lib

---
+++ genie/integrations/blockchain/svm/ts/examples/genie-nextjs-example/src/app/api/inbox/initialize/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Inbox } from "@genie-web3/svm-integration";
+import { getGenie } from "@/app/lib/genie";
+import { Keypair, PublicKey } from "@solana/web3.js";
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const initialAuth = Keypair.fromSecretKey(
+    Uint8Array.from(JSON.parse(body.initialAuth)),
+  );
+  console.log(initialAuth.publicKey)
+
+  const genie = await getGenie();
+
+  const inbox = new Inbox(genie, initialAuth.publicKey);
+  const txId = await inbox.initialize(initialAuth, body.platform, body.primaryKey);
+
+  return NextResponse.json({ success: true, txId: txId });
+}
