e960dabfc1286c8c8ba200735d211d302b90dec1
Deok <gyu.c301@gmail.com>
2023-11-09 02:11:09 +0900

    Add nft list fetching to deposit page

--- genie/integrations/blockchain/svm/ts/examples/genie-nextjs-example/src/app/api/inbox/getTokens/route.ts
+++ genie/integrations/blockchain/svm/ts/examples/genie-nextjs-example/src/app/api/inbox/getTokens/route.ts
@@ -1,25 +1,22 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Inbox } from "@genie-web3/svm-integration";
-import { getGenie } from "@/app/lib/genie";
-import { Keypair, PublicKey } from "@solana/web3.js";
+import { NextRequest, NextResponse } from 'next/server'
+import { Inbox } from '@genie-web3/svm-integration'
+import { getGenie } from '@/app/lib/genie'
+import { Keypair, PublicKey } from '@solana/web3.js'
 
 export async function POST(request: NextRequest) {
-    try{
-const body = await request.json();
-  const initialAuth = Keypair.fromSecretKey(
-    Uint8Array.from(JSON.parse(body.initialAuth)),
-  );
-  console.log(initialAuth.publicKey)
+  try {
+    const body = await request.json()
+    const initialAuth = Keypair.fromSecretKey(
+      Uint8Array.from(JSON.parse(body.initialAuth))
+    )
 
-  const genie = await getGenie();
+    const genie = await getGenie()
 
-  const inbox = new Inbox(genie, initialAuth.publicKey);
-  const list = await inbox.getTokens();
+    const inbox = new Inbox(genie, initialAuth.publicKey)
+    const list = await inbox.getTokens()
 
-  return NextResponse.json({ success: true, list: list });
-    }
-    catch(err){
-        return NextResponse.json({success: false, txId: JSON.stringify(err)})
-    }
-  
+    return NextResponse.json({ success: true, list: list })
+  } catch (err) {
+    return NextResponse.json({ success: false, txId: JSON.stringify(err) })
+  }
 }
--- genie/integrations/blockchain/svm/ts/examples/genie-nextjs-example/src/app/page.tsx
+++ genie/integrations/blockchain/svm/ts/examples/genie-nextjs-example/src/app/page.tsx
@@ -14,12 +14,22 @@ import {
   Transaction,
   SystemProgram
 } from '@solana/web3.js'
-import { getErrorMessage } from './utils'
+import { Metaplex } from '@metaplex-foundation/js'
+import { getErrorMessage, chunk } from './utils'
 
 export default function Home() {
   const [inboxKey, setInboxKey] = useState<string>('')
   const [tokenList, setTokenList] = useState<
     Array<{ mint: string; amount: string; decimals: string; selected: boolean }>
+  >([])
+  const [nftList, setNftList] = useState<
+    Array<{
+      mint: string
+      name: string
+      image: string
+      collection: string
+      selected: boolean
+    }>
   >([])
   const [amount, setAmount] = useState<string>('0')
 
@@ -56,7 +66,8 @@ export default function Home() {
         // Get the derived address of the destination wallet which will hold the custom token
         const associatedDestinationTokenAddr = getAssociatedTokenAddressSync(
           mintToken,
-          destPublicKey
+          destPublicKey,
+          true
         )
 
         const receiverAccount = await conn.connection.getAccountInfo(
@@ -92,7 +103,9 @@ export default function Home() {
         transaction.feePayer = wallet.publicKey
         wallet.sendTransaction(transaction, conn.connection)
       }
-    } catch (err) {}
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   const onChangeKey = (e: any) => {
@@ -147,11 +160,80 @@ export default function Home() {
       list.splice(0, 0, solanaBalance)
       setTokenList(list)
     }
+
+    const getNfts = async () => {
+      const metaplex = new Metaplex(conn.connection)
+      const list = await conn.connection
+        //@ts-ignore
+        .getParsedTokenAccountsByOwner(wallet.publicKey, {
+          programId: TOKEN_PROGRAM_ID
+        })
+        .then((res) =>
+          res.value
+            .filter((f) => {
+              return f.account.data.parsed.info.tokenAmount.decimals === 0
+            })
+            .filter(
+              (f) => f.account.data.parsed.info.tokenAmount.amount !== '0'
+            )
+            .map((v) => {
+              return {
+                mint: v.account.data.parsed.info.mint,
+                amount: v.account.data.parsed.info.tokenAmount.amount,
+                selected: false
+              }
+            })
+        )
+        .then((res) => {
+          const chunks = chunk(res, 100)
+
+          return Promise.all(
+            chunks.map(async (v) => {
+              const temp = await metaplex.nfts().findAllByMintList({
+                //@ts-ignore
+                mints: v.map((k) => new PublicKey(k.mint))
+              })
+              return temp
+            })
+          )
+        })
+        .then((res) => {
+          return res.flat()
+        })
+        .then((res) => {
+          return res.filter((v) => v !== null)
+        })
+        .then((res) => {
+          return Promise.all(
+            res.map((v) => {
+              //@ts-ignore
+              return metaplex.nfts().findByMetadata({ metadata: v?.address })
+            })
+          )
+        })
+        .then((res) => {
+          return res.map((v) => {
+            return {
+              mint: v.mint.address.toBase58() || '',
+              name: v.json?.name || '',
+              collection: v.collection?.address?.toBase58() || '',
+              image: v.json?.image || '',
+              selected: false
+            }
+          })
+        })
+        .catch((error) => {
+          throw new Error(getErrorMessage(error))
+        })
+      setNftList(list)
+    }
+
     getTokens()
+    getNfts()
   }, [wallet.publicKey])
 
-  const setSelectedToken = (i: number) => {
-    if (tokenList.length > i) {
+  const setSelectedToken = (i: number, nft: boolean) => {
+    if (tokenList.length > i && !nft) {
       tokenList.map((v, j) => {
         if (j === i) {
           v.selected = true
@@ -162,6 +244,17 @@ export default function Home() {
         }
       })
       setTokenList([...tokenList])
+    } else if (nftList.length > i && nft) {
+      nftList.map((v, j) => {
+        if (j === i) {
+          v.selected = true
+          return v
+        } else {
+          v.selected = false
+          return v
+        }
+      })
+      setNftList([...nftList])
     }
   }
 
@@ -181,7 +274,9 @@ export default function Home() {
           onClick={async () => {
             try {
               await sendToken(
-                tokenList.findLast((v) => v.selected === true)?.mint || ''
+                tokenList.findLast((v) => v.selected === true)?.mint ||
+                  nftList.findLast((v) => v.selected === true)?.mint ||
+                  ''
               )
             } catch (err) {
               alert('Token Not Selected')
@@ -209,12 +304,60 @@ export default function Home() {
                 height: '60px'
               }}
               onClick={() => {
-                setSelectedToken(i)
+                setSelectedToken(i, false)
               }}>
               <div style={{ width: '80%' }}>
                 <div>{`Mint Address : ${v.mint}`}</div>
                 <div>{`Current Amount : ${v.amount}`}</div>
                 <div>{`Decimals : ${v.decimals}`}</div>
+              </div>
+              <div
+                style={{
+                  width: '20%',
+                  height: '60px',
+                  display: 'flex',
+                  alignItems: 'center'
+                }}>
+                <input
+                  disabled={!v.selected}
+                  style={{}}
+                  placeholder="Enter Send Amount"
+                  value={v.selected ? amount : 'Not Selected'}
+                  pattern="\d*"
+                  onChange={(e) => {
+                    onChangeAmount(e.target.value)
+                  }}></input>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '12px',
+          marginTop: '24px'
+        }}>
+        {nftList.map((v, i) => {
+          return (
+            <div
+              key={i}
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                width: '100%',
+                border: v.selected ? 'solid 2px blue' : 'solid 0.5px grey',
+                height: '60px'
+              }}
+              onClick={() => {
+                setSelectedToken(i, true)
+              }}>
+              <Image src={v.image} alt={'nft image'} width={60} height={60} />
+              <div style={{ width: '80%' }}>
+                <div>{`Mint Address : ${v.mint}`}</div>
+                <div>{`collection : ${v.collection}`}</div>
+                <div>{`name : ${v.name}`}</div>
               </div>
               <div
                 style={{
