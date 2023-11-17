331c75bff696cab44999abb772f6fd861e17fe99
Deok <gyu.c301@gmail.com>
2023-11-17 11:44:18 +0900

    Add key output to example

--- genie/integrations/blockchain/svm/ts/examples/genie-nextjs-example/src/app/api/profile/initialize/route.ts
+++ genie/integrations/blockchain/svm/ts/examples/genie-nextjs-example/src/app/api/profile/initialize/route.ts
@@ -1,23 +1,30 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Profile } from "@genie-web3/svm-integration";
-import { getGenie } from "@/app/lib/genie";
-import { Keypair, PublicKey } from "@solana/web3.js";
+import { NextRequest, NextResponse } from 'next/server'
+import { Profile } from '@genie-web3/svm-integration'
+import { getGenie } from '@/app/lib/genie'
+import { Keypair, PublicKey } from '@solana/web3.js'
 
 export async function POST(request: NextRequest) {
-  try{ const body = await request.json();
-  const initialAuth = Keypair.fromSecretKey(
-    Uint8Array.from(JSON.parse(body.initialAuth)),
-  );
+  try {
+    const body = await request.json()
+    const initialAuth = Keypair.fromSecretKey(
+      Uint8Array.from(JSON.parse(body.initialAuth))
+    )
 
-  const genie = await getGenie();
+    const genie = await getGenie()
 
-  const profile = new Profile(genie, initialAuth.publicKey);
-  const txId = await profile.initialize(initialAuth);
-  console.log(txId);
+    const profile = new Profile(genie, initialAuth.publicKey)
+    const txId = await profile.initialize(initialAuth)
 
-  return NextResponse.json({ success: true, txId: txId });}
-  catch(err){
-    return NextResponse.json({success:false, txId: JSON.stringify(err)})
+    return NextResponse.json({
+      success: true,
+      txId: txId,
+      key: profile.key.toBase58()
+    })
+  } catch (err) {
+    return NextResponse.json({
+      success: false,
+      txId: JSON.stringify(err),
+      key: profile.key.toBase58()
+    })
   }
- 
 }
