b5afb51336fe01ca152265dbf10872e86d4d82c1
c301hoon <hoon.c301@gmail.com>
2023-11-24 13:36:16 +0900

    Add: api calls for register command
     #35

--- genie/integrations/sns/discord/Genie-DiscordBot/utils/api_call.py
+++ genie/integrations/sns/discord/Genie-DiscordBot/utils/api_call.py
@@ -1,7 +1,7 @@
 import os
-import requests
 import json
-
+import requests
+from utils.constants import BACKEND_ENDPOINT
 
 def check_user_account(discriminator):
     open_bracket = '{'
@@ -78,12 +78,87 @@ def create_inbox_account(discord_id, network_name):
 
     return data
 
+def check_user_account(discriminator):
+    open_bracket = '{'
+    close_bracket = '}'
+    body = f"""
+        query {open_bracket}
+            checkUserAccount (
+                snsName: "Discord"
+                discriminator: "{discriminator}"
+                networkName: "Solana"
+            ) {open_bracket}
+                socialAccountPubKey
+                inbox
+            {close_bracket}
+        {close_bracket}
+        """
+    response = requests.post(url=os.environ['BACKEND_ENDPOINT'], json={"query": body})
+    data = json.loads(response.text)
+    
+    try:
+        social_account_pub_key = data['data']['checkUserAccount']['socialAccountPubKey']
+        inbox = data['data']['checkUserAccount']['inbox']
+    except:
+        return None
+
+    return social_account_pub_key, inbox
+
+def create_social_account(nickname):
+    open_bracket = '{'
+    close_bracket = '}'
+    body = f"""
+        mutation {open_bracket}
+            createSocialAccount (
+                nickname: "{nickname}"
+            ) {open_bracket}
+                success
+                pubKey
+            {close_bracket}
+        {close_bracket}
+        """
+    response = requests.post(url=os.environ['BACKEND_ENDPOINT'], json={"query": body})
+    data = json.loads(response.text)
+     
+    try:
+        data = data['data']['createSocialAccount']['pubKey']
+    except:
+        return None
+
+    return data
+
+def create_inbox_account(discord_id, network_name):
+    open_bracket = '{'
+    close_bracket = '}'
+    body = f"""
+        mutation {open_bracket}
+            createInboxAccount (
+                discriminator: "{discord_id}"
+                snsName: "Discord"
+                networkName: "{network_name}"
+            ) {open_bracket}
+                success
+                walletAddress
+            {close_bracket}
+        {close_bracket}
+        """
+
+    response = requests.post(url=os.environ['BACKEND_ENDPOINT'], json={"query": body})
+    data = json.loads(response.text)
+
+    try:
+        data = data['data']['createInboxAccount']['walletAddress']
+    except:
+        return None
+
+    return data
+
 def get_user_inbox_account(discord_id):
     open_bracket = '{'
     close_bracket = '}'
     body = f"""
         query {open_bracket}
-            getUserInboxAccount (
+            getUserInboxWallet (
                 discriminator: "{discord_id}"
                 snsName: "Discord"
                 networkName: "Solana"
@@ -95,9 +170,38 @@ def get_user_inbox_account(discord_id):
 
     response = requests.post(url=os.environ['BACKEND_ENDPOINT'], json={"query": body})
     data = json.loads(response.text)
+    
     try:
-        data = data['data']['getUserInboxAccount']['walletAddress']
+        data = data['data']['getUserInboxWallet']['walletAddress']
     except:
         return None
 
     return data
+
+def register_sns(network_name, pub_key, discord_id, nickname):
+    open_bracket = '{'
+    close_bracket = '}'
+    body = f"""
+        mutation {open_bracket}
+            registerSns (
+                discriminator: "{discord_id}"
+                pubKey: "{pub_key}"
+                snsName: "Discord"
+                networkName: "{network_name}"
+                handle: "{nickname}"
+            ) {open_bracket}
+                success
+            {close_bracket}
+        {close_bracket}
+        """
+
+    response = requests.post(url=os.environ['BACKEND_ENDPOINT'], json={"query": body})
+    data = json.loads(response.text)
+
+    try:
+        data = data['data']['registerSns']['success']
+    except:
+        return None
+
+    return
+>>>>>>> c7925a8 (Add: api calls for register command)
