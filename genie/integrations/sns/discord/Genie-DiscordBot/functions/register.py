10ef32f0d27999285941533f0ff708c0fe9b595c
c301hoon <hoon.c301@gmail.com>
2023-11-24 13:33:38 +0900

    Add: !register command
     #35

--- genie/integrations/sns/discord/Genie-DiscordBot/functions/register.py
+++ genie/integrations/sns/discord/Genie-DiscordBot/functions/register.py
@@ -1,6 +1,10 @@
 import os
 from discord.ext import commands
+<<<<<<< HEAD
 from utils.api_call import check_user_account, get_user_inbox_account, create_inbox_account, create_social_account
+=======
+from utils.api_call import check_user_account, get_user_inbox_account, create_inbox_account, create_social_account, register_sns
+>>>>>>> d5e9a38 (Add: !register command)
 
 
 class Register(commands.Cog):
@@ -9,31 +13,44 @@ class Register(commands.Cog):
 
     @commands.command()
     async def register(self, ctx) -> None:
-        from_id = ctx.author.id
+        from_id = str(ctx.author.id)
         from_name = ctx.author.name
         from_discriminator = ctx.author.discriminator
         from_avatar = ctx.author.avatar
+
+        social_account_pub_key, is_inbox =check_user_account(from_id)
         
-        is_social_account, is_inbox =check_user_account(from_id)
-        
-        if is_social_account and is_inbox:
+        if social_account_pub_key != "" and is_inbox:
             inbox_address = get_user_inbox_account(from_id)
+            
             await ctx.reply(
                 f":genie:: Hey {from_name}! You already registered genie. Your inbox wallet address is {inbox_address}. This message will be deleted after 10 seconds.",
                 delete_after=10.0,
             )
+
+            return
         
-        if is_social_account:
+        await ctx.reply(
+                f":genie:: Please wait.",
+                delete_after=10.0,
+            )
+
+        if social_account_pub_key != "":
+            register_sns("Solana", social_account_pub_key, from_id, from_name + '#' + from_discriminator)
             inbox_address = create_inbox_account(from_id, "Solana")
+            
             await ctx.reply(
                 f":genie:: Hey {from_name}! Your inbox wallet is registered .\nAddress: {inbox_address}.\nThis message will be deleted after 10 seconds.",
             delete_after=10.0,
             )
+
         else:
-            create_social_account(from_name + '#' + from_discriminator)
+            social_account_pub_key = create_social_account(from_name + '#' + from_discriminator)
+            register_sns("Solana", social_account_pub_key, from_id, from_name + '#' + from_discriminator)
             inbox_address = create_inbox_account(from_id, "Solana")
+
             await ctx.reply(
-                f":genie:: Hey {from_name}! Your genie wallet is registered .\nInbox Address: {inbox_address}.\nThis message will be deleted after 10 seconds.",
+                f":genie:: Hey {from_name}! Your genie wallet is registered .\n**Inbox Address: {inbox_address}**\nThis message will be deleted after 10 seconds.",
             delete_after=10.0,
             )
 
